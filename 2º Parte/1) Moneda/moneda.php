a2a30b931ea7fa6e03641160ad28f3660d17aeb9
Carlos Escolar Calero <94523888+carluzox1999@users.noreply.github.com>
2022-11-21 21:41:55 +0000

    Moneda Completado

--- 2º Parte/1) Moneda/moneda.php
+++ 2º Parte/1) Moneda/moneda.php
@@ -1,65 +1,98 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="./CSS/bootstrap.min.css">
     <title>Moneda</title>
 </head>
+
 <body>
+    <style>
+        @keyframes rotar {
+            0% {
+                transform: rotateY(0);
+            }
+
+            100% {
+                transform: rotateY(2160deg);
+            }
+        }
+    </style>
+
     <script type="text/javascript">
-        var imagenes = ['./IMG/moenda-cara.png', './IMG/moenda-cruz.png'];
+        // Moneda
+        let cara = 0;
+        let cruz = 0;
 
-        let contador = 0;
-
-        function crearMoneda(){
-            var img = document.createElement('img');
-            img.scr = imagenes[contador];
-            document.getElementById('content').appendChild(img);
+        function actualizarConteoMoneda(cara, cruz) {
+            setTimeout(function () {
+                document.getElementById("cont-cara").innerText = "Cara: " + cara;
+                document.getElementById("cont-cruz").innerText = "Cruz: " + cruz;
+            }, 3000);
         }
 
-        function lanzarMoneda(){
-            if(contador <= 6) {
-                var img = document.getElementById('content').getElementsByTagName('img')[0];
+        function botonLanzar() {
+            setTimeout(function () {
+                btn.disabled = false;
+            }, 3000);
+        }
 
-                contador = contador % imagenes.length;
-                img.src = imagenes[contador];
+        function lanzarMoneda() {
+            let resultado = Math.floor(Math.random() * 2);
+            let moneda = document.getElementById("moneda");
+            moneda.style.animation = "none";
+
+            if (resultado == 0) {
+                cara++;
+                setTimeout(function () {
+                    document.getElementById("moneda").src = "./IMG/moenda-cara.png";
+                }, 2300);
+            } else {
+                cruz++;
+                setTimeout(function () {
+                    document.getElementById("moneda").src = "./IMG/moenda-cruz.png";
+                }, 2300);
             }
-            // contador++;
-            
+            setTimeout(function () {
+                moneda.style.animation = "rotar 3s forwards";
+            }, 100);
+
+            botonLanzar();
+            actualizarConteoMoneda(cara, cruz);
         }
     </script>
-    <div>
-        <h1>Cara o cruz</h1>
-    </div>
+
     <div class="container">
-        <h4>Elije una opción</h4>
-        <form action="moneda.php" method="post">
-            <div class="form-check">
-                <label class="form-check-label" for="cara" value="Cara">Cara</label>
-                <input class="form-check-input" type="radio" name="cara" id="cara">
-            </div>
-            <div class="form-check">
-                <label class="form-check-label" for="cruz" value="Cruz">Cruz</label>
-                <input class="form-check-input" type="radio" name="cruz" id="cruz">
-            </div>
-        </form>
-        <br>
-    </div>
-    <div class="container">
-        <div class="row">
-            <button type="button" onclick="lanzarMoneda()">
-                <div class="col-12" id="content">
-                    <span>
-                        <img src="./IMG/moenda-cara.png" alt="Moneda">
-                    </span>
-                </div>
-            </button>
+        <div class="d-flex justify-content-center align-items-center">
+            <h1 class="text-center">CARA / CRUZ</h1>
         </div>
-        <h2></h2>
+
+        <div class='btn-group gap-0 d-md-flex justify-content-end' role='group'>
+            <button id="btn_lanzar" class="btn btn-primary" onclick="lanzarMoneda()">Lanzar</button>
+            <button class="btn btn-primary" onClick="window.location.reload();">Refrescar Página</button>
+        </div>
+
+        <div class="d-flex justify-content-center">
+            <div id="moneda" class="col-4">
+                <img id="moneda" alt="moneda" src="./IMG/moenda-cara.png" />
+            </div>
+        </div>
+        <div class="mt-4 d-flex justify-content-center">
+            <div class="row">
+                <p id="cont-cara" class="bg-light rounded">
+                    Cara: 0
+                </p>
+                <p id="cont-cruz" class="bg-light rounded">
+                    Cruz: 0
+                </p>
+            </div>
+
+        </div>
     </div>
 
-    <script src="./JS/bootstrap.min.js"></script>
+    <script src="./JS/bootstrap.bundle.js"></script>
 </body>
 </html>
--- 2º Parte/2) Piedra papel tijera/juego.php
+++ 2º Parte/2) Piedra papel tijera/juego.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,9 +8,174 @@
     <link rel="stylesheet" href="./CSS/bootstrap.min.css">
     <title>Juego</title>
 </head>
-<body>
-    
 
-    <script src="./JS/bootstrap.min.js"></script>
+<body>
+    <style>
+        body {
+            background-color: #E0E0E0;
+        }
+
+        .form-group {
+            text-align: center;
+        }
+
+        #jugar {
+            width: 30%;
+        }
+
+        #resultado {
+            margin-top: 30px;
+        }
+    </style>
+
+    <script type="text/javascript">
+        // let opciones = ["piedra", "papel", "tijera"];
+        // let jugador = document.querySelectorAll("input[name=opciones]");
+        // let boton = document.getElementById("jugar");
+        // let resultado = document.getElementById("resultado");
+        // let maquina = "";
+        // let contador = 0;
+
+        // boton.addEventListener("click", function() {
+        // for (let i = 0; i < jugador.length; i++) {
+        //     if (jugador[i].checked) {
+        //     if (contador < 20) {
+        //         contador++;
+        //     } else {
+        //         contador = 0;
+        //         maquina = opciones[Math.floor(Math.random() * opciones.length)];
+        //         resultado.innerHTML = "La máquina ha elegido " + maquina;
+        //     }
+        //     }
+        // }
+        // });
+        document.getElementById("boton").addEventListener("click", function () {
+
+            let eleccionUsuario = document.querySelector("input[name=eleccion]:checked").value;
+
+            let eleccionMaquina = Math.floor(Math.random() * 3);
+
+            switch (eleccionMaquina) {
+                case 0:
+                    eleccionMaquina = "piedra";
+                    break;
+
+                case 1:
+                    eleccionMaquina = "papel";
+                    break;
+
+                case 2:
+                    eleccionMaquina = "tijera";
+                    break;
+            }
+
+            if (eleccionUsuario === eleccionMaquina) {
+                alert("Empate");
+            } else if (eleccionUsuario === "tijera" && eleccionMaquina === "papel") {
+                alert("Ganas");
+
+            } else if (eleccionUsuario === "tijera" && eleccionMaquina === "piedra") {
+                alert("Pierdes");
+
+            } else if (eleccionUsuario === "papel" && eleccionMaquina === "tijera") {
+                alert("Pierdes");
+
+            } else if (eleccionUsuario === "papel" && eleccionMaquina === "piedra") {
+                alert("Ganas");
+
+            } else if (eleccionUsuario === "piedra" && eleccionMaquina === "tijera") {
+                alert("Ganas");
+
+            } else if (eleccionUsuario === "piedra" && eleccionMaquina === "papel") {
+                alert("Pierdes");
+            }
+
+        });
+    </script>
+
+    <div class="container">
+        <!-- <div class="row">
+                <div class="col-sm-6 form-group">
+                    <h3>Usuario</h3>
+                    <div class="form-group">
+                        <label for="opciones">Selecciona una opción:</label>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="opciones"  value="piedra" checked>
+                            <label class="form-check-label" for="opciones">Piedra</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="opciones"  value="papel">
+                            <label class="form-check-label" for="opciones">Papel</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="opciones"  value="tijera">
+                            <label class="form-check-label" for="opciones">Tijera</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 form-group">
+                    <h3>Máquina</h3>
+                    <div class="form-group">
+                        <label for="opciones">Selecciona una opción:</label>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="opciones"  value="piedra" checked>
+                            <label class="form-check-label" for="opciones">Piedra</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="opciones"  value="papel">
+                            <label class="form-check-label" for="opciones">Papel</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="opciones"  value="tijera">
+                            <label class="form-check-label" for="opciones">Tijera</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        <div class="row">
+            <div class="col-sm-12 form-group text-center">
+                <button class="btn btn-success" id="jugar">¡Jugar!</button>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-12 form-group text-center">
+                <h2 id="resultado">Resultado</h2>
+            </div>
+        </div>
+    </div> -->
+        <div class="container">
+            <h1>Piedra papel y tijera</h1>
+            <form action="juego.php" method="post">
+                <div>
+                    <label for="tijera">Tijera</label>
+                    <input type="radio" id="tijera" name="eleccion" value="tijera">
+                </div>
+                <div>
+                    <label for="papel">Papel</label>
+                    <input type="radio" id="papel" name="eleccion" value="papel">
+                </div>
+                <div>
+                    <label for="piedra">Piedra</label>
+                    <input type="radio" id="piedra" name="eleccion" value="piedra">
+                </div>
+            </form>
+            <br>
+
+            <div class="imagenes">
+                <img src="https://img.freepik.com/vector-gratis/pegatina-tijera-purpura-sobre-fondo-blanco_1308-61999.jpg?w=2000"
+                    id="tijeraImg" width="10%">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Toiletpapier_%28Gobran111%29.jpg/1200px-Toiletpapier_%28Gobran111%29.jpg"
+                    id="papelImg" width="10%">
+                <img src="https://media.istockphoto.com/id/1288973456/es/foto/piedra-de-piedra-gris.jpg?s=612x612&w=0&k=20&c=P5-c3LzB_cXZI1uJ9S5ec2A77xI9qyMQX9U1JXG9fpE="
+                    id="piedraImg" width="10%">
+            </div>
+            <br>
+            <button id="boton" type="submit">Jugar</button>
+        </div>
+
+        <script src="./JS/bootstrap.min.js"></script>
 </body>
+
 </html>
