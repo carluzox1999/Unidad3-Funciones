92e2b29842dfc1c74045a9e323e8b454f2fcb209
Carlos Escolar Calero <94523888+carluzox1999@users.noreply.github.com>
2022-11-25 20:32:10 +0000

    Piedra papel tijera completado
    Ejercicio completado al 100%

--- 2º Parte/2) Piedra papel tijera/juego.php
+++ 2º Parte/2) Piedra papel tijera/juego.php
@@ -5,177 +5,255 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./CSS/bootstrap.min.css">
-    <title>Juego</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <title>Piedra, Papel, Tijera</title>
 </head>
 
 <body>
     <style>
-        body {
-            background-color: #E0E0E0;
-        }
+        @keyframes slide {
+            0% {
+                translate: 0;
+            }
 
-        .form-group {
-            text-align: center;
-        }
+            5% {
+                translate: -100%;
+            }
 
-        #jugar {
-            width: 30%;
-        }
+            10% {
+                translate: -200%;
+            }
 
-        #resultado {
-            margin-top: 30px;
+            15% {
+                translate: -100%;
+            }
+
+            20% {
+                translate: 0;
+            }
+
+            25% {
+                translate: -100%;
+            }
+
+            30% {
+                translate: -200%;
+            }
+
+            35% {
+                translate: -100%;
+            }
+
+            40% {
+                translate: 0;
+            }
+
+            45% {
+                translate: -100%;
+            }
+
+            50% {
+                translate: -200%;
+            }
+
+            55% {
+                translate: -100%;
+            }
+
+            60% {
+                translate: 0;
+            }
+
+            65% {
+                translate: -100%;
+            }
+
+            70% {
+                translate: -200%;
+            }
+
+            75% {
+                translate: -100%;
+            }
+
+            80% {
+                translate: 0;
+            }
+
+            85% {
+                translate: -100%;
+            }
+
+            90% {
+                translate: -200%;
+            }
+
+            95% {
+                translate: -100%;
+            }
+
+            100% {
+                translate: 0;
+            }
         }
     </style>
 
-    <script type="text/javascript">
-        // let opciones = ["piedra", "papel", "tijera"];
-        // let jugador = document.querySelectorAll("input[name=opciones]");
-        // let boton = document.getElementById("jugar");
-        // let resultado = document.getElementById("resultado");
-        // let maquina = "";
-        // let contador = 0;
+    <script>
+        let ordenJuego = { piedra: 0, papel: 1, tijera: 2 };
+        let eleccion = ["piedra", "papel", "tijera"];
 
-        // boton.addEventListener("click", function() {
-        // for (let i = 0; i < jugador.length; i++) {
-        //     if (jugador[i].checked) {
-        //     if (contador < 20) {
-        //         contador++;
-        //     } else {
-        //         contador = 0;
-        //         maquina = opciones[Math.floor(Math.random() * opciones.length)];
-        //         resultado.innerHTML = "La máquina ha elegido " + maquina;
-        //     }
-        //     }
-        // }
-        // });
-        document.getElementById("boton").addEventListener("click", function () {
+        let pGanadas = 0;
+        let pPerdidas = 0;
+        let pEmpatadas = 0;
 
-            let eleccionUsuario = document.querySelector("input[name=eleccion]:checked").value;
+        function selecBorrar() {
+            document.getElementById("0").classList.remove("border-primary");
+            document.getElementById("1").classList.remove("border-primary");
+            document.getElementById("2").classList.remove("border-primary");
+            document.getElementById("0").classList.add("border-dark");
+            document.getElementById("1").classList.add("border-dark");
+            document.getElementById("2").classList.add("border-dark");
+        }
 
-            let eleccionMaquina = Math.floor(Math.random() * 3);
+        function selectImage() {
+            selecBorrar();
+            let move = document.querySelector('input[name="juego"]:checked');
+            document.getElementById(move.value).classList.remove("border-dark");
+            document.getElementById(move.value).classList.add("border-primary");
+        }
 
-            switch (eleccionMaquina) {
-                case 0:
-                    eleccionMaquina = "piedra";
-                    break;
+        function orden() {
+            setTimeout(function () {
+                document.getElementById("maq_2").classList.remove("order-first");
+                document.getElementById("maq_1").classList.remove("order-first");
+                document.getElementById("maq_0").classList.remove("order-first");
+            }, 1000);
+        }
 
-                case 1:
-                    eleccionMaquina = "papel";
-                    break;
+        function juego() {
+            let resMaquina = Math.floor(Math.random() * 3);
+            let movMaquina = document.getElementById("movMaquina");
+            orden();
+            movMaquina.style.animation = "none";
+            setTimeout(function () {
+                movMaquina.style.animation = "slide 2s ease-out";
+            }, 100);
+            setTimeout(function () {
+                document.getElementById("maq_" + resMaquina).classList.add("order-first");
+            }, 1000);
+            comprobar_result(resMaquina);
+        }
 
-                case 2:
-                    eleccionMaquina = "tijera";
-                    break;
+        function comprobar_result(resMaquina) {
+            let move = document.querySelector('input[name="juego"]:checked').value;
+            if (move == resMaquina) {
+                pEmpatadas++;
             }
 
-            if (eleccionUsuario === eleccionMaquina) {
-                alert("Empate");
-            } else if (eleccionUsuario === "tijera" && eleccionMaquina === "papel") {
-                alert("Ganas");
-
-            } else if (eleccionUsuario === "tijera" && eleccionMaquina === "piedra") {
-                alert("Pierdes");
-
-            } else if (eleccionUsuario === "papel" && eleccionMaquina === "tijera") {
-                alert("Pierdes");
-
-            } else if (eleccionUsuario === "papel" && eleccionMaquina === "piedra") {
-                alert("Ganas");
-
-            } else if (eleccionUsuario === "piedra" && eleccionMaquina === "tijera") {
-                alert("Ganas");
-
-            } else if (eleccionUsuario === "piedra" && eleccionMaquina === "papel") {
-                alert("Pierdes");
+            // Si gana piedra
+            else if (move == ordenJuego["piedra"]) {
+                if (resMaquina == ordenJuego["tijera"]) {
+                    pGanadas++;
+                } else {
+                    pPerdidas++;
+                }
             }
 
-        });
+            // Si gana papel
+            else if (move == ordenJuego["papel"]) {
+                if (resMaquina == ordenJuego["piedra"]) {
+                    pGanadas++;
+                } else {
+                    pPerdidas++;
+                }
+            }
+
+            // Si gana tijera
+            else if (move == ordenJuego["tijera"]) {
+                if (resMaquina == ordenJuego["papel"]) {
+                    pGanadas++;
+                } else {
+                    pPerdidas++;
+                }
+            }
+
+            setTimeout(function () {
+                document.getElementById("contUsuario").innerText = pGanadas;
+                document.getElementById("contMaquina").innerText = pPerdidas;
+                document.getElementById("contEmpate").innerText = pEmpatadas;
+            }, 2100);
+        }
     </script>
 
-    <div class="container">
-        <!-- <div class="row">
-                <div class="col-sm-6 form-group">
-                    <h3>Usuario</h3>
-                    <div class="form-group">
-                        <label for="opciones">Selecciona una opción:</label>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="opciones"  value="piedra" checked>
-                            <label class="form-check-label" for="opciones">Piedra</label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="opciones"  value="papel">
-                            <label class="form-check-label" for="opciones">Papel</label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="opciones"  value="tijera">
-                            <label class="form-check-label" for="opciones">Tijera</label>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6 form-group">
-                    <h3>Máquina</h3>
-                    <div class="form-group">
-                        <label for="opciones">Selecciona una opción:</label>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="opciones"  value="piedra" checked>
-                            <label class="form-check-label" for="opciones">Piedra</label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="opciones"  value="papel">
-                            <label class="form-check-label" for="opciones">Papel</label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="opciones"  value="tijera">
-                            <label class="form-check-label" for="opciones">Tijera</label>
-                        </div>
-                    </div>
-                </div>
+    <div class="container p-5">
+        <div class="d-flex justify-content-center align-items-center">
+            <h1 class="text-center">
+                Piedra, papel y tijera
+            </h1>
+        </div>
+        <div class="d-flex justify-content-center">
+            <h6 class="fs-5 mx-3 p-2">
+                Usuario: <b id="contUsuario">0</b>
+            </h6>
+            <h6 class="fs-5 mx-3 p-2">
+                Máquina: <b id="contMaquina">0</b>
+            </h6>
+            <h6 class="fs-5 mx-3 p-2">
+                Empate: <b id="contEmpate">0</b>
+            </h6>
+        </div>
+        <div class="p-2 col-1 d-block mx-auto overflow-hidden">
+        <div id="movMaquina" class="d-flex">
+          <img
+            id="maq_0"
+            alt="maquina"
+            src="./IMG/piedra.png"
+            class="img-fluid border border-2 mx-2"
+          />
+          <img
+            id="maq_1"
+            alt="maquina"
+            src="./IMG/papel.png"
+            class="img-fluid border border-2 mx-2"
+          />
+          <img
+            id="maq_2"
+            alt="maquina"
+            src="./IMG/tijera.png"
+            class="img-fluid border border-2 mx-2"
+          />
+        </div>
+      </div>
+        <h6 class="col-8 mx-auto p-3 text-center bg-light rounded-bottom">
+            Elige tu movimiento
+        </h6>
+        <div class="col-8 mx-auto bg-light p-3 d-flex justify-content-center">
+            <!-- Eliguiendo piedra -->
+            <div class="col-2 mx-3 d-flex flex-column">
+                <label for="piedra"><img id="0" alt="piedra" src="./IMG/piedra.png"
+                        class="img-fluid border border-3" /></label>
+                <input type="radio" id="piedra" class="d-none" name="juego" value="0" onclick="selectImage()" />
             </div>
-
-        <div class="row">
-            <div class="col-sm-12 form-group text-center">
-                <button class="btn btn-success" id="jugar">¡Jugar!</button>
+            <!-- Eliguiendo papel -->
+            <div class="col-2 mx-3 d-flex flex-column">
+                <label for="papel"><img id="1" alt="papel" src="./IMG/papel.png"
+                        class="img-fluid border border-3" /></label>
+                <input type="radio" id="papel" class="d-none" name="juego" value="1" onclick="selectImage()" />
+            </div>
+            <!-- Eliguiendo tijera -->
+            <div class="col-2 mx-3 d-flex flex-column">
+                <label for="tijera"><img alt="tijera" id="2" src="./IMG/tijera.png"
+                        class="img-fluid border border-3" /></label>
+                <input type="radio" id="tijera" class="d-none" name="juego" value="2" onclick="selectImage()" />
             </div>
         </div>
-
-        <div class="row">
-            <div class="col-sm-12 form-group text-center">
-                <h2 id="resultado">Resultado</h2>
-            </div>
-        </div>
-    </div> -->
-        <div class="container">
-            <h1>Piedra papel y tijera</h1>
-            <form action="juego.php" method="post">
-                <div>
-                    <label for="tijera">Tijera</label>
-                    <input type="radio" id="tijera" name="eleccion" value="tijera">
-                </div>
-                <div>
-                    <label for="papel">Papel</label>
-                    <input type="radio" id="papel" name="eleccion" value="papel">
-                </div>
-                <div>
-                    <label for="piedra">Piedra</label>
-                    <input type="radio" id="piedra" name="eleccion" value="piedra">
-                </div>
-            </form>
-            <br>
-
-            <div class="imagenes">
-                <img src="https://img.freepik.com/vector-gratis/pegatina-tijera-purpura-sobre-fondo-blanco_1308-61999.jpg?w=2000"
-                    id="tijeraImg" width="10%">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Toiletpapier_%28Gobran111%29.jpg/1200px-Toiletpapier_%28Gobran111%29.jpg"
-                    id="papelImg" width="10%">
-                <img src="https://media.istockphoto.com/id/1288973456/es/foto/piedra-de-piedra-gris.jpg?s=612x612&w=0&k=20&c=P5-c3LzB_cXZI1uJ9S5ec2A77xI9qyMQX9U1JXG9fpE="
-                    id="piedraImg" width="10%">
-            </div>
-            <br>
-            <button id="boton" type="submit">Jugar</button>
-        </div>
-
-        <script src="./JS/bootstrap.min.js"></script>
+        
+        <div class="mt-4 d-flex justify-content-center">
+            <button id="btn_lanzar" class="mx-2 col-4 btn btn-primary" onclick="juego()">
+                Jugar
+            </button>
+        </div> 
+    </div>
 </body>
 
 </html>
